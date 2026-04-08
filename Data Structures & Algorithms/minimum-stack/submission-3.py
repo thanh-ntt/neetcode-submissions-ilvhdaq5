48675683c5a3abc7db749c25f3fec2b4f4b12840
Thanh Nguyen <36496138+thanh-ntt@users.noreply.github.com>
2026-04-09 00:44:17 +0800

    Add: minimum-stack - submission-3

---
+++ Data Structures & Algorithms/minimum-stack/submission-3.py
@@ -0,0 +1,24 @@
+from collections import deque, defaultdict
+import heapq
+
+class MinStack:
+
+    def __init__(self):
+        self.stack: list[int] = []
+        self.min_stack: list[int] = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if not self.min_stack or val <= self.min_stack[-1]:
+            self.min_stack.append(val)
+
+    def pop(self) -> None:
+        val = self.stack.pop()
+        if val == self.min_stack[-1]:
+            self.min_stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.min_stack[-1]
