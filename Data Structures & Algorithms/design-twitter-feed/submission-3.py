d43ece5f0bec74f449eb8f6c7c982b16941124c4
Thanh Nguyen <36496138+thanh-ntt@users.noreply.github.com>
2026-04-14 14:48:22 +0800

    Add: design-twitter-feed - submission-3

---
+++ Data Structures & Algorithms/design-twitter-feed/submission-3.py
@@ -0,0 +1,29 @@
+from collections import defaultdict
+
+
+class Twitter:
+
+    def __init__(self):
+        self.posts = defaultdict(list)
+        self.follows = defaultdict(set)
+        self.ts = 0
+
+    def postTweet(self, userId: int, tweetId: int) -> None:
+        self.posts[userId].append((tweetId, self.ts))
+        self.ts += 1
+
+    def getNewsFeed(self, userId: int) -> List[int]:
+        nf = list(self.posts[userId])
+        for followee in self.follows[userId]:
+            nf.extend(self.posts[followee])
+        nf.sort(key=lambda t: t[1], reverse=True)
+        return [t[0] for t in nf][:10]
+
+    def follow(self, followerId: int, followeeId: int) -> None:
+        if followerId != followeeId:
+            self.follows[followerId].add(followeeId)
+
+    def unfollow(self, followerId: int, followeeId: int) -> None:
+        if followerId != followeeId:
+            self.follows[followerId].discard(followeeId)
+        
